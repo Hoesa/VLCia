d8e024f360bb1a715c1c3bb58aa2f81ea197eaa8
Hoesa <slalometje@gmail.com>
2019-07-19 07:59:10 +0200

    Initial Commit

---
+++ MainWindow.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Gtk;
+using LibVLCSharp.Shared;
+using LibVLCSharp.GTK;
+
+public partial class MainWindow : Gtk.Window
+
+{
+    MediaPlayer mediaPlayer; //mediaplayer
+    bool mediaplay = false;  //bool to check if something is already playing
+    bool mediapause = false; //bool to check if media is paused
+    string stream; //file to stream
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Gtk.Application.Quit();
+        a.RetVal = true;
+    }
+
+    //Play media
+    protected void Play(object sender, EventArgs e)
+    {
+        if (choose.Active == -1)
+        { txt.Text = "Choose first :)"; }
+        else if (mediaplay == false)
+        {
+
+            using (var libvlc = new LibVLC())
+            {
+                mediaPlayer = new MediaPlayer(libvlc);
+                stream = choose.ActiveText;
+
+                // Make videoview and add 
+                VideoView mediav = new VideoView { MediaPlayer = mediaPlayer };
+
+                VLCia.MainClass.win.Add(mediav);
+                VLCia.MainClass.win.ShowAll();
+
+                //start video
+                using (var media = new Media(libvlc,
+                    "http://localhost:9980/renter/stream/" + stream,
+                    FromType.FromLocation))
+                {
+                    mediaPlayer.Play(media);
+                }
+
+                mediaplay = true;
+                txt.Text = "Playing";
+                System.Threading.Thread.Sleep(5000);
+                scale.SetRange(0, Convert.ToDouble(mediaPlayer.Length));
+                scale.Sensitive = true;
+                mediaPlayer.TimeChanged += MediaPlayer_TimeChanged1;
+
+
+            }
+        }
+
+        else if (mediaplay == true)
+        {
+            mediaPlayer.Pause();
+            if (mediapause == true)
+            { txt.Text = "Pause"; mediapause = true; }
+            else
+            { txt.Text = "Playing"; mediapause = false; }
+        }
+
+    }
+
+    //Pause
+    protected void Pause(object sender, EventArgs e)
+    {
+        mediaPlayer.Pause();
+
+        if (mediapause == false)
+        { txt.Text = "Pause"; mediapause = true; }
+        else
+        { txt.Text = "Playing"; mediapause = false; }
+    }
+
+    //Stop
+    protected void Stop(object sender, EventArgs e)
+    {
+        mediaPlayer.Stop();
+        txt.Text = "Stopped";
+        mediaplay = false;
+        scale.Sensitive = false;
+        scale.Value = 0;
+    }
+
+    //Refresh
+    protected async void Refresh(object sender, EventArgs e)
+    {
+        await VLCia.MainClass.RefresAsync();
+        VLCia.MainClass.Process();
+
+        foreach (string media in VLCia.MainClass.mediafiles)
+        {
+            choose.AppendText(media);
+        }
+
+        refresh.Sensitive = false;
+
+    }
+
+    //Copy stream URL to Clipboard
+    protected void Url(object sender, EventArgs e)
+    {
+        if (choose.Active == -1)
+        { }
+        else
+        {
+            string stream = choose.ActiveText;
+            Gtk.Clipboard clipboard = Gtk.Clipboard.Get(Gdk.Atom.Intern("CLIPBOARD", false));
+            clipboard.Text = "http://localhost:9980/renter/stream/" + stream;
+            txt.Text = "http://localhost:9980/renter/stream/" + stream;
+        }
+    }
+
+    //Combobox input change while playing
+    protected void Changed(object sender, EventArgs e)
+    {
+        if (mediaplay == true)
+        {
+            mediaPlayer.Stop();
+            mediaplay = false;
+            txt.Text = "Stopped";
+        }
+    }
+
+    //Set scale value according to mediaplayer time
+    void MediaPlayer_TimeChanged1(object sender, MediaPlayerTimeChangedEventArgs e)
+    {
+        scale.Value = mediaPlayer.Time;
+    }
+
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Gtk;
+using System.IO;
+using System.Net.Http;
+using System.Collections.Generic;
+
+namespace VLCia
+{
+    class MainClass
+    {
+      
+        public static MainWindow win; 
+        public static String json; //Json response string
+        public static string split = "},{"; //Where to split data of sia files in the json
+        public static string cutafter = "\",\"stuck\""; //cuting data not needed after this point
+        public static string cutbefore = "\"siapath\":\""; //cuting data not needed before this point
+        public static List<string> mediafiles = new List<string>(); //List of media files on sia available for download
+
+        public static string[] mediaExtensions = {
+       ".MOV",".MKV",".MP4",".3G2",".3GA","3GP",".AAC",".ADT",".ANX",".AVCHD","AVI",".BIK",
+       ".F4V",".FLV",".GXF",".H264",".HDMOV",".M1V","M2V","M4A",".M4B",".M4V",".MKA",".MKS",
+       ".MP3","..MPEG1",".MPEG4",".MPG2",".MPV",".MTS",".NSV",".NUV",".OGG",".OGV",".OMA",
+       ".OPUS",".PSS",".RA",".RBS",".REC",".RM",".RMI",".RT",".SPX",".SVI",".TOD",".TRP",
+       ".TS",".TTA",".VLT",".VOC","VP6","VQF","VRO",".WAV",".WEBM",".WV",".XESC",".ZAB"
+};
+
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            win = new MainWindow();
+            win.ShowAll();
+            Application.Run();
+        }
+
+        //Refresh, can only be done once each instance as I have no idea how to clear the array used in process
+        public static async System.Threading.Tasks.Task RefresAsync()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), "http://localhost:9980/renter/files?cached=false"))
+                {
+                    request.Headers.TryAddWithoutValidation("User-Agent", "Sia-Agent");           
+                    HttpResponseMessage response = await httpClient.SendAsync(request);
+                    response.EnsureSuccessStatusCode();                                 
+                    json = await response.Content.ReadAsStringAsync();
+                }
+            }
+        }
+
+        //where the magic happens
+        public static void Process()
+        {
+            mediafiles.Clear();
+            string[] files = json.Split(new[] { split }, StringSplitOptions.None);
+
+            foreach (string s in files)
+            {
+                bool b = s.Contains("\"available\":true");
+                   
+                if (b == true)
+                { 
+                    string path = s.Substring(0, s.LastIndexOf(cutafter) + 0);
+                    path = path.Substring(path.LastIndexOf(cutbefore));
+                    path = path.Replace("\"siapath\":\"", "");
+
+                    bool c = IsMediaFile(path);
+
+                    if (c == true)
+                    {
+                        mediafiles.Add(path);
+                    }
+                }
+            }
+        }
+
+        //Check if file is a media file
+        static bool IsMediaFile(string path)
+        {
+            return -1 != Array.IndexOf(mediaExtensions, Path.GetExtension(path).ToUpperInvariant());
+        }
+    }
+    }
+
+
+
+
+
